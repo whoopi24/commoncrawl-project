6b9430c02b636e8ce02d2f1a339d35b6c76d0859
whoopi24 <marina.whoopi@gmail.com>
2024-02-18 00:00:04 +0100

    get_files.py: added functions create_text_corpus() and preprocess_text_corpus() -> regarding efficiency, maybe combine these two functions and avoid saving unnecessary text data

--- get_files.py
+++ get_files.py
@@ -1,12 +1,18 @@
 # import packages
 from urllib.request import urlretrieve
-import time
+import glob
 import os
 import re
 import random
+import gzip
+import json
+from collections import Counter
+import time
+from warcio import ArchiveIterator
+from langdetect import detect
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 # function to recursively retry (10 times) to download a file
 def tryDownload(url, filename, retries=0):
@@ -17,40 +23,124 @@ def tryDownload(url, filename, retries=0):
         urlretrieve(url, filename)
     except:
         time.sleep(1)
-        tryDownload(url, filename, retries+1)
+        tryDownload(url, filename, retries + 1)
 
-# function to create text corpus of specific crawl and for specific top-level domain
-def get_files(crawl_name, tld='^at,'):
+
+# function to get wet files of specific crawl and for specific top-level domain
+def get_files(crawl_name, top_lvl_domain='at'):
     # download cluster.idx file for this crawl
     path1 = 'https://data.commoncrawl.org/cc-index/collections/'
-    path2 = '/indexes/cluster.idx'
+    path2 = '/indexes/'
     path_ccrawl = path1 + crawl_name + path2
     url = path_ccrawl + 'cluster.idx'
-    crawl_dir = os.path.join("S:", "msommer", crawl_name)
+    crawl_dir = os.path.join("S:", "msommer", crawl_name, top_lvl_domain)
     if not os.path.exists(crawl_dir):
         os.makedirs(crawl_dir)
-    filename = os.path.join(crawl_dir, "cluster.txt")
-    tryDownload(url, filename)
+    cluster_file = os.path.join(crawl_dir, "cluster.txt")
+    tryDownload(url, cluster_file)
 
     # filter cdx files with top-level-domain (tld) at or de
-    with open(filename, "rt") as file:
+    regex = '^' + top_lvl_domain + ','
+    with open(cluster_file, "rt") as file:
         cdx_files = []
         for line in file:
             tmp = line.split("\t")
-            match = re.search(tld, tmp[0])
+            match = re.search(regex, tmp[0])
             if match:
                 if not tmp[1] in cdx_files:
                     cdx_files.append(tmp[1])
 
-    # choose and download cdx files, e.g. cdx-00003.gz -> way too much for de
-    if len(cdx_files) > 3:
-        idx = random.sample(range(0, len(cdx_files)), 3)
+    # choose only one cdx file
+    if len(cdx_files) > 1:
+        idx = random.sample(range(0, len(cdx_files)), 1)
         cdx_files = [cdx_files[i] for i in idx]
 
+    # download cdx files
     for file in cdx_files:
-        print(file)
         url = path_ccrawl + file
         filename = os.path.join(crawl_dir, file)
         tryDownload(url, filename)
+        print("Successfully downloaded" + file)
+
+    # get correct wet files
+    wet_files = []
+    for file in cdx_files:
+        warc_files = []
+        filename = os.path.join(crawl_dir, file)
+        with gzip.open(filename, 'rt') as f:
+            for line in f:
+                match = re.search(regex, line)
+                if match:
+                    l = line.split("{\"url\":")
+                    string = "{\"url\":" + l[1]
+                    d = json.loads(string)
+                    warc = d["filename"]
+                    if int(d["length"]) > 10000:
+                        warc_files.append(warc)
+            count_dict = Counter(warc_files)
+
+            # only download wet files with a lot of occurrences
+            for key, value in count_dict.items():
+                if value >= 50:
+                    key = key.replace("/warc/", "/wet/").replace("warc.gz", "warc.wet.gz")
+                    key_path = key.split("/")[-1]
+                    filename = os.path.join(crawl_dir, key_path)
+                    if filename not in wet_files:
+                        wet_files.append(filename)
+                        url = "https://data.commoncrawl.org/" + key
+                        print(url)
+                        tryDownload(url, filename)
+    print("Download of wet files finished.")
+    return wet_files
 
 
+# function to create text corpus of specific crawl and for specific top-level domain
+def create_text_corpus(crawl_name, top_lvl_domain='at'):  # ToDo: maybe pre-processing before saving unnecessary lines ?
+    crawl_dir = os.path.join("S:", "msommer", crawl_name, top_lvl_domain)
+    os.chdir(crawl_dir)
+    fnames = []
+    for w in glob.glob("*.warc.wet.gz"):
+        fname = w.replace(".warc.wet.gz", "-text.txt")
+        with open(w, 'rb') as stream, open(fname, 'wb') as f:
+            for record in ArchiveIterator(stream):
+                if record.rec_type == 'conversion':
+                    regex = '\.' + top_lvl_domain + '/'
+                    match = re.search(regex, record.rec_headers.get_header('WARC-Target-URI'))
+                    length = int(record.rec_headers.get_header('Content-Length'))
+                    rec_type = record.rec_headers.get_header('Content-Type')
+                    if match and length > 10000 and rec_type == "text/plain":
+                        print(record.rec_headers.get_header('WARC-Target-URI'))
+                        content = record.content_stream().read()
+                        f.write(content)
+        fnames.append(fname)
+    print("All wet files successfully pre-processed.")
+    return fnames
+
+
+# function to preprocess text corpus (.txt file)
+def preprocess_text_corpus(crawl_name, top_lvl_domain, fnames):
+    final_fname = os.path.join("S:", "msommer", crawl_name, top_lvl_domain, "text_corpus.txt")
+    with open(final_fname, 'wt', encoding="utf-8") as f:
+        for fname in fnames:
+            with open(fname, "rt", encoding="utf-8") as file:
+                for line in file:
+                    if line.count('.') < 2:
+                        continue
+                    try:
+                        lang = detect(line)
+                    except:
+                        lang = "none"
+                    if lang != "de":
+                        continue
+                    f.write(line)
+
+
+if __name__ == '__main__':
+    import time
+    start_time = time.clock()
+    crawl_name = 'CC-MAIN-2013-20'   # take a small crawl for testing
+    top_lvl_domain = 'at'
+    wet_files = get_files(crawl_name, top_lvl_domain)
+    fnames = create_text_corpus(wet_files, crawl_name, top_lvl_domain)
+    preprocess_text_corpus(crawl_name, top_lvl_domain, fnames)
+    print(time.clock() - start_time, "minutes")
